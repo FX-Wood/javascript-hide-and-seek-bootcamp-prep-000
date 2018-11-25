e36b88c1c5e9048009d5686d869cecf600f5804f
FX Wood <fxwood2@gmail.com>
2018-11-25 23:04:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function deepestChild() {
 function increaseRankBy(n)  {
   const FirstRankedList = document.querySelector('ul.ranked-list')
   const FirstRankedListInitLength = FirstRankedList.childElementCount
-  for (i = FirstRankedListInitLength + 1; i < n+FirstRankedListInitLength+1; i++) {
+  for (let i = FirstRankedListInitLength + 1; i < n+FirstRankedListInitLength+1; i++) {
     const newLi = document.createElement("li");
     const newLiText = document.createTextNode(`${i}`)
     newLi.appendChild(newLiText);
