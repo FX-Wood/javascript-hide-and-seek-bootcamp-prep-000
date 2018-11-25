46576dee7c02c7a4a212e91ff119e9081e21f4d7
FX Wood <fxwood2@gmail.com>
2018-11-25 22:28:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector("#nested .target");
+}
+
+function deepestChild() {
+  let grandNode = document.getElementById("grand-node");
+  let grandNodeChildren = grandNode.querySelectorAll("*");
+  return grandNodeChildren[grandNodeChildren.length-1]
+}
+
+function increaseRankBy
