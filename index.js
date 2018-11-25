6e70c3e1810550c523970aebdbec99fabc9e8efa
FX Wood <fxwood2@gmail.com>
2018-11-25 23:53:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function increaseRankBy(n)  {
     for (let childIndex = 0; childIndex < childItems.length; childIndex++) {
       console.log('secondloop')
       let current = childItems[childIndex].innerText;
-      childItems[childIndex].innerText = toString(parseInt(current) + n)
+      childItems[childIndex].innerText = toString(parseInt(current) + 3)
     }
   }
 }
