da877e360c5d9ac9a385b2f8b9341fffcbd76798
FX Wood <fxwood2@gmail.com>
2018-11-26 00:26:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector("#nested .target");
+}
+
+function deepestChild() {
+  const grandNode = document.getElementById("grand-node");
+  const grandNodeChildren = grandNode.querySelectorAll("*");
+  return grandNodeChildren[grandNodeChildren.length-1]
+}
+
+function increaseRankBy(n)  {
+  const domLists = document.querySelectorAll('.ranked-list');
+  for (let listIndex = 0; listIndex < domLists.length; listIndex++) {
+    console.log('firstloop')
+    let childItems = domLists[listIndex].children
+    for (let childIndex = 0; childIndex < childItems.length; childIndex++) {
+      console.log('secondloop')
+      let current = childItems[childIndex].innerHTML;
+      childItems[childIndex].innerHTML = parseInt(current) + n
+    }
+  }
+}
